ec480642698aa5553e78ca7c138cd9014a8e363c
hiab gebremichael <hiab.gebremichael.stu.ocsb.ca>
2025-05-03 17:50:12 +0000

    finding_pairs.py

--- finding_pairs.py
+++ finding_pairs.py
@@ -1,3 +1,8 @@
 def main():
-    print("Welcome! this program will tell you all the pairs of positive two digit numbers whose sum is 60.")
-    
+    print(
+        "Welcome! this program will tell you all the pairs of positive two digit numbers whose sum is 60."
+    )
+
+
+for num1 in range(10, 100):
+    num2 = 10
